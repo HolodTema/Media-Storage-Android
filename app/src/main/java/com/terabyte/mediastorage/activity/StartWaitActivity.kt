fd722a56cec682439d3b5e07be40418cfcf02bdf
ArtemHolodilov <art.holodilov@gmail.com>
2024-09-11 01:14:38 +0800

    Now StartWaitActivity controls the choice between LoginActivity and MainActivity when the user starts the app.

--- app/src/main/java/com/terabyte/mediastorage/activity/StartWaitActivity.kt
+++ app/src/main/java/com/terabyte/mediastorage/activity/StartWaitActivity.kt
@@ -1,17 +1,32 @@
 package com.terabyte.mediastorage.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.terabyte.mediastorage.activity.ui.theme.MediaStorageTheme
+import com.terabyte.mediastorage.util.DataStoreManager
 
 class StartWaitActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MediaStorageTheme {
+            DataStoreManager.readFromDataStore(applicationContext, DataStoreManager.Keys.ACCESS_TOKEN) {
+                if(it==null) {
+                    startActivity(
+                        Intent(applicationContext, LoginActivity::class.java)
+                            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                            .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                    )
+                }
+                else {
+                    startActivity(
+                        Intent(applicationContext, MainActivity::class.java)
+                            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                            .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                    )
+                }
             }
         }
     }
--- app/src/main/java/com/terabyte/mediastorage/app/MyApplication.kt
+++ app/src/main/java/com/terabyte/mediastorage/app/MyApplication.kt
@@ -1,30 +1,11 @@
 package com.terabyte.mediastorage.app
 
 import android.app.Application
-import android.content.Intent
-import com.terabyte.mediastorage.activity.LoginActivity
-import com.terabyte.mediastorage.activity.MainActivity
-import com.terabyte.mediastorage.util.DataStoreManager
 
 class MyApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
-        DataStoreManager.readFromDataStore(applicationContext, DataStoreManager.Keys.ACCESS_TOKEN) {
-            if(it==null) {
-                startActivity(
-                    Intent(applicationContext, LoginActivity::class.java)
-                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                )
-            }
-            else {
-                startActivity(
-                    Intent(applicationContext, MainActivity::class.java)
-                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                )
-            }
-        }
+
     }
 }
